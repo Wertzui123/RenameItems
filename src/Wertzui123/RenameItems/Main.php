0058a518480b48edc37894c38c8c70e518a817b3
Wertzui123 <46199283+Wertzui123@users.noreply.github.com>
2019-06-15 14:15:23 +0200

    Add files via upload

---
+++ src/Wertzui123/RenameItems/Main.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Wertzui123\RenameItems;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use Wertzui123\RenameItems\commands\rename;
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnable() : void{
+	    $this->ConfigUpdater(1.0);
+	    $this->getServer()->getCommandMap()->register("RenameItems", new rename($this));
+	}
+
+public function Config(){
+	    $cfg = new Config($this->getDataFolder()."config.yml", 2);
+	    return $cfg;
+}
+
+public function getMSGS(){
+	    $msgs = new Config($this->getDataFolder()."messages.yml", 2);
+	    return $msgs;
+}
+
+public function ConfigUpdater($version){
+	    $cfgpath = $this->getDataFolder()."config.yml";
+	    $msgpath = $this->getDataFolder()."messages.yml";
+	    if(file_exists($cfgpath)){
+	        $cfgversion = $this->Config()->get("version");
+	        if($cfgversion !== $version){
+	            $this->getLogger()->info("Your config has been renamed to config-".$cfgversion.".yml and your messages file has been renamed to messages-".$cfgversion.".yml. That's because your config version wasn't the latest avable. So we created a new config and a new messages file for you!");
+	            rename($cfgpath, $this->getDataFolder()."config-".$cfgversion.".yml");
+                rename($msgpath, $this->getDataFolder()."messages-".$cfgversion.".yml");
+                $this->saveResource("config.yml");
+                $this->saveResource("messages.yml");
+	        }
+        }else{
+            $this->saveResource("config.yml");
+            $this->saveResource("messages.yml");
+        }
+}
+
+}
---
+++ src/Wertzui123/RenameItems/commands/rename.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wertzui123\RenameItems\commands;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use Wertzui123\RenameItems\Main;
+use pocketmine\Player;
+
+class rename extends Command
+{
+
+    private $plugin;
+    private $config;
+
+    public function __construct(Main $plugin)
+    {
+        $config = $plugin->Config()->getAll();
+        parent::__construct($config["command"] ?? "rename", $config["command_description"] ?? "Change item names", null, $config["command_aliases"] ?? []);
+        $this->setPermission("renameitems.rename.cmd");
+        $this->plugin = $plugin;
+        $this->config = $config;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        $msgs = $this->plugin->getMSGS()->getAll();
+        if ($sender instanceof Player) {
+            if ($sender->hasPermission($this->getPermission())) {
+                $item = $sender->getInventory()->getItemInHand();
+                if (!isset($args[0])) {
+                    $sender->sendMessage($this->config["usage"]);
+                    return;
+                }
+                if ($item->getId() == 0) {
+                    $sender->sendMessage($msgs["hold_item"]);
+                    return;
+                }
+                $item->setCustomName(implode(" ", $args));
+                $sender->getInventory()->setItemInHand($item);
+                $sender->sendMessage(str_replace("{name}", implode(" ", $args), $msgs["succes"]));
+            }else{
+                $sender->sendMessage($msgs["no_permission"]);
+            }
+        }else{
+            $sender->sendMessage($msgs["run_ingame"]);
+        }
+    }
+}
