2bbaa2f933a0f1a735163f5a31bf5cc9906f8477
Wertzui123 <46199283+Wertzui123@users.noreply.github.com>
2020-06-05 18:17:01 +0200

    RenameItems 2.0 is here
    This update added:
    
    - Config updater
    - API
    - UI
    - /block command

--- src/Wertzui123/RenameItems/Main.php
+++ src/Wertzui123/RenameItems/Main.php
@@ -2,45 +2,121 @@
 
 namespace Wertzui123\RenameItems;
 
-use pocketmine\event\player\PlayerJoinEvent;
+use jojoe77777\FormAPI\CustomForm;
+use pocketmine\item\Item;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
 use pocketmine\utils\Config;
+use Wertzui123\RenameItems\commands\block;
 use Wertzui123\RenameItems\commands\rename;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase
+{
 
-	public function onEnable() : void{
-	    $this->ConfigUpdater(1.1);
-	    $this->getServer()->getCommandMap()->register("RenameItems", new rename($this));
-	    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-	}
+    /** @var float */
+    const CONFIG_VERSION = 2.0;
 
-public function Config(){
-	    $cfg = new Config($this->getDataFolder()."config.yml", 2);
-	    return $cfg;
-}
+    /** @var Config */
+    private $stringsFile;
 
-public function getMSGS(){
-	    $msgs = new Config($this->getDataFolder()."messages.yml", 2);
-	    return $msgs;
-}
+    public function onEnable(): void
+    {
+        $this->ConfigUpdater();
+        $this->stringsFile = new Config($this->getDataFolder() . "strings.yml", Config::YAML);
+        $this->getServer()->getCommandMap()->register("RenameItems", new rename($this));
+        $this->getServer()->getCommandMap()->register("RenameItems", new block($this));
+    }
 
-public function ConfigUpdater($version){
-	    $cfgpath = $this->getDataFolder()."config.yml";
-	    $msgpath = $this->getDataFolder()."messages.yml";
-	    if(file_exists($cfgpath)){
-	        $cfgversion = $this->Config()->get("version");
-	        if($cfgversion !== $version){
-	            $this->getLogger()->info("Your config has been renamed to config-".$cfgversion.".yml and your messages file has been renamed to messages-".$cfgversion.".yml. That's because your config version wasn't the latest avable. So we created a new config and a new messages file for you!");
-	            rename($cfgpath, $this->getDataFolder()."config-".$cfgversion.".yml");
-                rename($msgpath, $this->getDataFolder()."messages-".$cfgversion.".yml");
-                $this->saveResource("config.yml");
-                $this->saveResource("messages.yml");
-	        }
-        }else{
+    /**
+     * Returns a string from the strings file
+     * @param string $key
+     * @param array $replace [optional]
+     * @return string
+     */
+    public function getString($key, $replace = []){
+        return str_replace(array_keys($replace), $replace, $this->stringsFile->getNested($key));
+    }
+
+    /**
+     * Returns a message from the strings file
+     * @see Main::getString()
+     * @param string $key
+     * @param array $replace [optional]
+     * @return string
+     */
+    public function getMessage($key, $replace = []){
+        return $this->getString($key, $replace);
+    }
+
+    /**
+     * @api
+     * Checks whether the given item is blocked (can't be renamed)
+     * @param Item $item
+     * @return bool
+     */
+    public function isBlocked(Item $item){
+        return in_array($item->getId(), $this->getConfig()->get('blocked_items')) || ($item->getNamedTag()->hasTag("RenameItems") && $item->getNamedTag()->getString("RenameItems") === "blocked");
+    }
+
+    /**
+     * @api
+     * Blocks the given item from being renamed
+     * @param Item $item
+     * @param bool $unblock [optional]
+     */
+    public function block(Item &$item, $unblock = false){
+        $unblock ? $item->getNamedTag()->removeTag("RenameItems") : $item->getNamedTag()->setString("RenameItems", "blocked");
+    }
+
+    /**
+     * @api
+     * Opens an ui to rename the item in the players hand
+     * @param bool $permissionCheck [optional]
+     * @param Player $player
+     */
+    public function openRenameUI(Player $player, $permissionCheck = true){
+        $form = new CustomForm(function (Player $player, $data) use ($permissionCheck){
+            if(is_null($data)) return;
+            if ($permissionCheck && !$player->hasPermission("renameitems.cmd.rename")) {
+                $player->sendMessage($this->getMessage('command.rename.noPermission'));
+                return;
+            }
+            $item = $player->getInventory()->getItemInHand();
+            if ($item->isNull()) {
+                $player->sendMessage($this->getMessage('command.rename.noItem'));
+                return;
+            }
+            if ($this->isBlocked($item) && !$player->hasPermission("renameitems.blockeditems.bypass")) {
+                $player->sendMessage($this->getMessage('command.rename.blocked'));
+                return;
+            }
+            $item->setCustomName($data['name']);
+            $player->getInventory()->setItemInHand($item);
+            $player->sendMessage($this->getMessage('command.rename.success', ["{name}" => $data['name']]));
+        });
+        $form->setTitle($this->getString('ui.rename.title'));
+        $form->addInput($this->getString('ui.rename.description'), $this->getString('ui.rename.replacement'), '', 'name');
+        $player->sendForm($form);
+    }
+
+    /**
+     * Checks whether the config version is the latest and updates it if it isn't
+     */
+    private function ConfigUpdater()
+    {
+        if (!file_exists($this->getDataFolder() . "config.yml")) {
+            $this->saveResource('config.yml');
+            $this->saveResource('strings.yml');
+            return;
+        }
+        if ($this->getConfig()->get('config-version') !== self::CONFIG_VERSION) {
+            $config_version = $this->getConfig()->get('config-version');
+            $this->getLogger()->info("§eYour Config isn't the latest. RenameItems renamed your old config to §bconfig-" . $config_version . ".yml §6and created a new config. Have fun!");
+            rename($this->getDataFolder() . "config.yml", $this->getDataFolder() . "config-" . $config_version . ".yml");
+            rename($this->getDataFolder() . "strings.yml", $this->getDataFolder() . "strings-" . $config_version . ".yml");
             $this->saveResource("config.yml");
             $this->saveResource("messages.yml");
         }
-}
+    }
+
 }
--- src/Wertzui123/RenameItems/commands/rename.php
+++ src/Wertzui123/RenameItems/commands/rename.php
@@ -6,51 +6,54 @@ namespace Wertzui123\RenameItems\commands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\plugin\Plugin;
 use Wertzui123\RenameItems\Main;
 use pocketmine\Player;
 
-class rename extends Command
+class rename extends Command implements PluginIdentifiableCommand
 {
 
     private $plugin;
-    private $config;
 
     public function __construct(Main $plugin)
     {
-        $config = $plugin->Config()->getAll();
-        parent::__construct($config["command"] ?? "rename", $config["command_description"] ?? "Change item names", null, $config["command_aliases"] ?? []);
-        $this->setPermission("renameitems.rename.cmd");
+        parent::__construct($plugin->getConfig()->getNested("command.rename.command"), $plugin->getConfig()->getNested("command.rename.description"), $plugin->getConfig()->getNested("command.rename.usage"), $plugin->getConfig()->getNested("command.rename.aliases"));
+        $this->setPermission("renameitems.cmd.rename");
         $this->plugin = $plugin;
-        $this->config = $config;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args)
     {
-        $msgs = $this->plugin->getMSGS()->getAll();
-        if ($sender instanceof Player) {
-            if ($sender->hasPermission($this->getPermission())) {
-                $item = $sender->getInventory()->getItemInHand();
-                if (!isset($args[0])) {
-                    $sender->sendMessage($this->config["usage"]);
-                    return;
-                }
-                if ($item->getId() == 0) {
-                    $sender->sendMessage($msgs["hold_item"]);
-                    return;
-                }
-                if(in_array($item->getId(), $this->config["banned_items"]) && !$sender->hasPermission("renameitems.blockeditems.bypass")){
-                    $sender->sendMessage($msgs["item_banned"]);
-                    return;
-                }
-
-                $item->setCustomName(implode(" ", $args));
-                $sender->getInventory()->setItemInHand($item);
-                $sender->sendMessage(str_replace("{name}", implode(" ", $args), $msgs["succes"]));
-            }else{
-                $sender->sendMessage($msgs["no_permission"]);
-            }
-        }else{
-            $sender->sendMessage($msgs["run_ingame"]);
+        if (!$sender instanceof Player) {
+            $sender->sendMessage($this->plugin->getMessage('command.rename.runIngame'));
+            return;
         }
+        if (!$sender->hasPermission($this->getPermission())) {
+            $sender->sendMessage($this->plugin->getMessage('command.rename.noPermission'));
+            return;
+        }
+        $item = $sender->getInventory()->getItemInHand();
+        if ($item->isNull()) {
+            $sender->sendMessage($this->plugin->getMessage('command.rename.noItem'));
+            return;
+        }
+        if ($this->plugin->isBlocked($item) && !$sender->hasPermission("renameitems.blockeditems.bypass")) {
+            $sender->sendMessage($this->plugin->getMessage('command.rename.blocked'));
+            return;
+        }
+        if (!isset($args[0])) {
+            $this->plugin->openRenameUI($sender);
+            return;
+        }
+        $item->setCustomName(implode(" ", $args));
+        $sender->getInventory()->setItemInHand($item);
+        $sender->sendMessage($this->plugin->getMessage('command.rename.success', ["{name}" => implode(" ", $args)]));
     }
+
+    public function getPlugin(): Plugin
+    {
+        return $this->plugin;
+    }
+
 }
